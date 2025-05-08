ab88fffead5a67366722aea3a8c1796f580bad17
HyeonJooooo <la0503ab@gmail.com>
2025-05-08 22:58:32 +0900

    :sparkles: home 화면 UI 완료

---
+++ feature/home/src/main/java/com/example/home/HomeViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.home
+
+import androidx.lifecycle.ViewModel
+
+data class ExchangeRateData(
+    val fromCurrency: String,  // ex: "KRW"
+    val toCurrency: String,    // ex: "USD"
+    val rate: Double           // ex: 0.00068
+)
+
+data class PriceItemData(
+    val name: String,
+    val localPrice: Int,
+    val localCurrency: String,
+    val convertedPrice: Double,
+    val convertedCurrency: String
+)
+
+class HomeViewModel : ViewModel() {
+
+    val exchangeRate = ExchangeRateData("KRW", "USD", 0.00068)
+
+    val priceList = listOf(
+        PriceItemData("A Bottle of Water", 800, "KRW", 0.54, "USD"),
+        PriceItemData("Coffee", 4200, "KRW", 3.12, "USD"),
+        PriceItemData("Taxi Base Fare", 3800, "KRW", 2.81, "USD")
+    )
+}
